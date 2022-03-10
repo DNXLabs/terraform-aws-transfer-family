62b75c7c29c7a0bf901ec4a2007826147ec08bc5
Bruno da Silva Valenga <brunodasilvalenga@gmail.com>
2022-03-10 14:56:19 -0300

    chage policy based on user type

--- iam.tf
+++ iam.tf
@@ -50,7 +50,9 @@ data "aws_iam_policy_document" "s3_access_for_sftp_users" {
       "s3:PutObjectACL"
     ]
 
-    resources = [
+    resources = each.value.is_admin == true ? [
+      "${join("", data.aws_s3_bucket.landing[*].arn)}/*"
+    ] : [
       "${join("", data.aws_s3_bucket.landing[*].arn)}/${each.value.username}/*"
     ]
   }
